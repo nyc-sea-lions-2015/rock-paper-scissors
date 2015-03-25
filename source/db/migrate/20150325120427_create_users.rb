1b479dc8004b2504df4f4e446c03eaec84d983fd
Dan Asselin <dasselin86@gmail.com>
2015-03-25 14:24:25 -0400

    Drop games migration file, fill in token, user_token and user migration files.

--- source/db/migrate/20150325120427_create_users.rb
+++ source/db/migrate/20150325120427_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name, null: false, limit: 100
+
       t.timestamps null: false
     end
   end
--- source/db/migrate/20150325120455_create_tokens.rb
+++ source/db/migrate/20150325120455_create_tokens.rb
@@ -1,4 +1,10 @@
 class CreateTokens < ActiveRecord::Migration
   def change
+    create_table :tokens do |t|
+      t.string :name, null: false, limit: 56
+      t.string :filepath, null: false
+
+      t.timestamps null: false
+    end
   end
 end
