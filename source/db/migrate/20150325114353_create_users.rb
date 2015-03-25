374b627741660b163eaf9dcf3bfee4be5d386a58
Tracy Teague <tracy.teague05@gmail.com>
2015-03-25 12:09:31 -0400

    Edit migration file for users.

--- source/db/migrate/20150325114353_create_users.rb
+++ source/db/migrate/20150325114353_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    t.string :name, null: false
+    create_table :users do |t|
+      t.string :name, null: false
 
-    t.timestamps
+      t.timestamps
+    end
   end
 end
