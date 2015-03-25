9bde093f613805a694d35b5e652069b859a60c83
suprfrye <tara.c.frye@gmail.com>
2015-03-25 16:31:06 -0400

    Add game post and show routes

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -5,3 +5,17 @@ end
 get '/game' do
   erb :game
 end
+
+post '/game' do
+  @game = Game.new(params)
+  if @game.save
+    redirect "/games/#{@game.id}"
+  else
+    redirect '/'
+  end
+end
+
+get '/game/:id' do
+  @game = Game.find_by(id: params[:id])
+  erb :show
+end
