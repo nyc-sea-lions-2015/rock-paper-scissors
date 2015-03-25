aadab3cebf94fa3162100da4361cef952a0957b6
Dan Asselin <dasselin86@gmail.com>
2015-03-25 12:09:17 -0400

    Write migration file for users

--- source/db/migrate/20150325120427_create_users.rb
+++ source/db/migrate/20150325120427_create_users.rb
@@ -1,4 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :name, null: false, limit: 100
+
+      t.timestamps null: false
+    end
   end
 end
+
