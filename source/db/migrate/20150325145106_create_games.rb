96a0b11a2c841a60a29f23bfe960e8205aef965d
Dan Asselin <dasselin86@gmail.com>
2015-03-25 15:07:56 -0400

    Write game migration file again

--- source/db/migrate/20150325145106_create_games.rb
+++ source/db/migrate/20150325145106_create_games.rb
@@ -1,5 +1,12 @@
 class CreateGames < ActiveRecord::Migration
   def change
+    create_table :games do |t|
+      t.integer :user_1_id, null: false
+      t.integer :user_2_id, null: false
+      t.integer :user_1_token_id, null: false
+      t.integer :user_2_token_id, null: false
 
+      t.timestamps null: false
+    end
   end
 end
