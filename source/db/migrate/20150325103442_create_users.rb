33c3c2d9077192f8299d67424c72befdcd79f886
suprfrye <tara.c.frye@gmail.com>
2015-03-25 11:03:11 -0400

    Add user and symbol models and migrations

---
+++ source/db/migrate/20150325103442_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false, limit: 30
+      t.boolean :win
+      has_one :symbol
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ source/db/migrate/20150325103502_create_symbols.rb
@@ -0,0 +1,4 @@
+class CreateSymbols < ActiveRecord::Migration
+  def change
+  end
+end
