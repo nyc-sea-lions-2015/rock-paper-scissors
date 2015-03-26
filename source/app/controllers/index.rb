c2b39908294fda98cf33488d84bad3969e19c9a7
Dan Asselin <dasselin86@gmail.com>
2015-03-25 21:22:00 -0400

    Add variables to game route and render in game.erb

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -3,11 +3,13 @@ get '/' do
 end
 
 get '/game' do
-
+  player_1 = User.find_by(name: "Player 1")
+  player_2 = User.find_by(name: "Player 2")
+  @all_players = [player_1, player_2]
   rock = Token.find_by(name: "rock")
   paper = Token.find_by(name: "paper")
   scissors = Token.find_by(name: "scissors")
-  all_tokens = [rock, paper, scissors]
+  @all_tokens = [rock, paper, scissors]
 
-  erb :game, locals: {tokens: all_tokens}
+  erb :game
 end
