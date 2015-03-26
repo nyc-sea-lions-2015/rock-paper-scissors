438dda59ab95296d6b31de331eedc9af17d147f4
Dan Asselin <dasselin86@gmail.com>
2015-03-25 20:48:34 -0400

    create seeds.rb

---
+++ source/db/seeds.rb
@@ -0,0 +1,9 @@
+player_1 = User.create!(name:"Player 1")
+player_2 = User.create!(name:"Player 2")
+
+rock = Token.create!(name:"rock", url:"http://www.internetgardener.co.uk/uploadimages/600/d24aeb87-630a-479d-a6cd-2f7a338c54fe.jpg")
+
+paper = Token.create!(name:"paper", url:"http://cdn.dickblick.com/items/102/40/10240-1009-1-3ww-l.jpg")
+
+scissors = Token.create!(name:"scissors", url:"http://www.redorbit.com/media/uploads/2004/10/25_365e8c1575c16e3d93d7f99c73a20162.jpg")
+
