9430baf6122b641f1ace2878af9e852e1e34c929
sabron13 <kurt.schnelker@gmail.com>
2015-03-25 20:06:24 -0400

    sets up views and two gets routes

--- source/app/models/gamelogic.rb
+++ source/app/models/gamelogic.rb
@@ -1,5 +1,27 @@
 class Gamelogic
-  def initialize(gameobj)
-    @gameobj = gameobj
+  def initialize(token_one, token_two)
+    @token_one = token_one
+    @token_two = token_two
   end
+
+  def game
+    hand = [token_one, token_two].join('_').to_sym
+
+    rock_outcomes = { Rock_Rock: "Draw", Rock_Paper: "Lose", Rock_Scissors: "Win" }
+    paper_outcomes = { Paper_Paper: "Draw", Paper_Scissors: "Lose", Paper_Rock: "Win" }
+    scissors_outcomes = { Scissors_Scissors: "Draw", Scissors_Rock: "Lose", Scissors_Paper: "Win"}
+
+    case token_one
+    when "Rock"
+      rock_outcomes[hand]
+    when "Paper"
+      paper_outcomes[hand]
+    when "Scissors"
+      scissors_outcomes[hand]
+    else
+      return
+    end
+
+  end
+
 end
