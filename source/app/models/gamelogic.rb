2b1f9bbecec08e34c2839863ffec24748107a580
sabron13 <kurt.schnelker@gmail.com>
2015-03-26 17:45:43 -0400

    Gamelogic works on server

--- source/app/models/gamelogic.rb
+++ source/app/models/gamelogic.rb
@@ -5,13 +5,13 @@ class Gamelogic
   end
 
   def game
-    hand = [token_one, token_two].join('_').to_sym
+    hand = [@token_one, @token_two].join('_').to_sym
 
     rock_outcomes = { rock_rock: "Draw", rock_paper: "Player 2 Wins", rock_scissors: "Player 1 Wins" }
     paper_outcomes = { paper_paper: "Draw", paper_scissors: "Player 2 Wins", paper_rock: "Player 1 Wins" }
     scissors_outcomes = { scissors_scissors: "Draw", scissors_rock: "Player 2 Wins", scissors_paper: "Player 1 Wins"}
 
-    case token_one
+    case @token_one
     when "rock"
       rock_outcomes[hand]
     when "paper"
