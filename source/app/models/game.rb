bf3dfb9c5e813d2d176ce712125cee4200cab3f6
Dan Asselin <dasselin86@gmail.com>
2015-03-26 16:01:27 -0400

    Create working #winner method in Game model

--- source/app/models/game.rb
+++ source/app/models/game.rb
@@ -4,11 +4,28 @@ class Game < ActiveRecord::Base
   belongs_to :player_1_token, :class_name => 'Token'
   belongs_to :player_2_token, :class_name => 'Token'
 
-  def winner
 
-    if player_1_token.name == 'rock' &&
-      player_2_token.name == 'scissors'
-      return player_1
+  def winner
+    #player 1 winning scenarios
+    if (player_1_token.name == 'rock' &&
+        player_2_token.name == 'scissors') ||
+       (player_1_token.name == 'paper' &&
+        player_2_token.name == 'rock') ||
+       (player_1_token.name == 'scissors' &&
+        player_2_token.name == 'paper')
+       return player_1
+    elsif
+    #player 2 winning scenarios
+       (player_2_token.name == 'rock' &&
+        player_1_token.name == 'scissors') ||
+       (player_2_token.name == 'paper' &&
+        player_1_token.name == 'rock') ||
+       (player_2_token.name == 'scissors' &&
+        player_1_token.name == 'paper')
+       return player_2
+    #ties
+    else
+      "Draw."
     end
 
   end
