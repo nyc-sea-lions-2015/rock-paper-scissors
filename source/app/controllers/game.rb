68bf808a0fd118af4e6cc84315faa0e666d30ae1
Dan Asselin <dasselin86@gmail.com>
2015-03-26 18:32:00 -0400

    Working game, no js or css integration

--- source/app/controllers/game.rb
+++ source/app/controllers/game.rb
@@ -1,4 +1,5 @@
 get '/game' do
+  cur_winner = params[:winner]
   player_1 = User.find_by(name: "Player 1")
   player_2 = User.find_by(name: "Player 2")
   all_players = [player_1, player_2]
@@ -7,9 +8,35 @@ get '/game' do
   scissors = Token.find_by(name: "scissors")
   all_tokens = [rock, paper, scissors]
 
-  erb :game, locals: {tokens: all_tokens, players: all_players}
+  erb :game, locals: {
+                      tokens: all_tokens,
+                      players: all_players,
+                      winner: cur_winner
+                     }
 end
 
 post '/game' do
+
+  player_1_token = Token.find_by(name: params[:player_1_token_name])
+  player_2_token = Token.find_by(name: params[:player_2_token_name])
+  cur_game = Game.new(
+                  player_1_id: 1,
+                  player_2_id: 2,
+                  player_1_token_id: player_1_token.id,
+                  player_2_token_id: player_2_token.id
+                 )
+
+  if cur_game.winner.is_a?(User)
+    cur_winner = "#{cur_game.winner.name.gsub(' ', '')}"
+  else
+    cur_winner = cur_game.winner
+  end
+
+
+  if cur_game.save
+    redirect "/game?winner=#{cur_winner}"
+  else
+    [404, "Invalid game!"]
+  end
 
 end
