75dce62b2ed1f787cdd013cb4d66d7b634da2727
Dan Asselin <dasselin86@gmail.com>
2015-03-26 16:27:23 -0400

    Fix bug in game post route

---
+++ source/app/controllers/game.rb
@@ -0,0 +1,15 @@
+get '/game' do
+  player_1 = User.find_by(name: "Player 1")
+  player_2 = User.find_by(name: "Player 2")
+  all_players = [player_1, player_2]
+  rock = Token.find_by(name: "rock")
+  paper = Token.find_by(name: "paper")
+  scissors = Token.find_by(name: "scissors")
+  all_tokens = [rock, paper, scissors]
+
+  erb :game, locals: {tokens: all_tokens, players: all_players}
+end
+
+post '/game' do
+
+end
--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -2,19 +2,3 @@ get '/' do
   erb :index
 end
 
-get '/game' do
-  player_1 = User.find_by(name: "Player 1")
-  player_2 = User.find_by(name: "Player 2")
-  @all_players = [player_1, player_2]
-  rock = Token.find_by(name: "rock")
-  paper = Token.find_by(name: "paper")
-  scissors = Token.find_by(name: "scissors")
-  @all_tokens = [rock, paper, scissors]
-
-  erb :game
-end
-
-post '/game/:id' do
-  puts params
-
-end
