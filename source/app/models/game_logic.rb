61e2004dd913d5776fb9a0cb40f35b45c0f7815f
Eveanandi <butler.eveanandi@gmail.com>
2015-03-26 11:33:22 -0400

    Update all models to properly define table relationships

--- source/app/models/game_logic.rb
+++ source/app/models/game_logic.rb
@@ -4,7 +4,15 @@ class GameLogic
 # Paper = 2
 # Scissor = 3
 
+# RULES = {
+#   "rock" => "scissor",
+#   "paper" => "rock",
+#   "scissor" => "paper"
+# }
 
+RULES = {
+
+}
   def initalize(player_1_id, player_2_id, player_1_token_id, player_2_token_id)
     @player_1_id = player_1_id
     @player_2_id = player_2_id
--- source/app/models/token.rb
+++ source/app/models/token.rb
@@ -1,3 +1,4 @@
 class Token < ActiveRecord::Base
- belongs_to :game
+has_many :wins, :class_name => 'Game', :foreign_key => 'winning_token_id'
+has_many :loses, :class_name => 'Game', :foreign_key => 'losing_token_id'
 end
