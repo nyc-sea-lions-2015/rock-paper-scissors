eab9c90f9a1eece3d946b1249b1a09157df482e1
Malcolm Bouzi <mrbouzi@gmail.com>
2015-03-26 11:03:48 -0400

    models

--- source/app/models/game.rb
+++ source/app/models/game.rb
@@ -1,5 +1,14 @@
 class Game < ActiveRecord::Base
 	belongs_to :player_1, :class_name => 'User'
 	belongs_to :player_2, :class_name => 'User'
+  belongs_to :player_1_choice, :class_name => 'Token'
+  belongs_to :player_2_choice, :class_name => 'Token'
 
+
+
+  def winner
+    return nil unless player_2_choice and player_2_choice
+    return player_1 if player_1_choice.beats(player_2_choice)
+    return player_2 if player_2_choice.beats(player_1_choice)
+  end
 end
--- source/app/models/token.rb
+++ source/app/models/token.rb
@@ -4,4 +4,13 @@ class Token < ActiveRecord::Base
 	validates :name, presence: true
 	validates :image, presence: true
 
+  BEATS = {
+    'rock' => 'scissors',
+    'paper' => 'rock',
+    'scissors' => 'paper'
+  }
+
+  def beats(opponent_choice)
+    BEATS[name] == opponent_choice.name
+  end
 end
