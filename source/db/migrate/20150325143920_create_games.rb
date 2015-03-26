8693aeeca78f36d9f87982312290dfc08090ddda
suprfrye <tara.c.frye@gmail.com>
2015-03-26 11:45:45 -0400

    Update games column names

--- source/db/migrate/20150325143920_create_games.rb
+++ source/db/migrate/20150325143920_create_games.rb
@@ -1,10 +1,10 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.integer :winner, null: false
-      t.integer :loser, null: false
-      t.integer :winner_token, null: false
-      t.integer :loser_token, null: false
+      t.integer :winner_id, null: false
+      t.integer :loser_id, null: false
+      t.integer :winner_token_id, null: false
+      t.integer :loser_token_id, null: false
 
       t.timestamps null: false
     end
