aaf6548b37e2f1657d67973c01a789830eba7a5a
Steven G. Harms <steven@devbootcamp.com>
2015-03-29 21:32:38 -0400

    Update language to use token over symbol in Game migration

--- source/db/migrate/2015029202400_create_games.rb
+++ source/db/migrate/2015029202400_create_games.rb
@@ -3,8 +3,8 @@ class CreateGames < ActiveRecord::Migration
     create_table :games do |t|
       t.integer :north_player_id, null: false
       t.integer :south_player_id, null: false
-      t.integer :north_symbol_id, null: false
-      t.integer :south_symbol_id, null: false
+      t.integer :north_token_id, null: false
+      t.integer :south_token_id, null: false
 
       t.integer :winning_player_id
       t.integer :losing_player_id
