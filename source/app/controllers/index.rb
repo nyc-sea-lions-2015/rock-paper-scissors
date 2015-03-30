3caa84e2556b703f150df21acd5947119a6ad58c
Steven G. Harms <steven@devbootcamp.com>
2015-03-29 21:34:49 -0400

    Record /throw_1 -> /throw_2

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -5,3 +5,14 @@ end
 get '/throw_1' do
   erb :throw_1
 end
+
+post '/record_throw_1' do
+  redirect "/throw_2/#{params[:player_id]}/#{params[:token_id]}"
+end
+
+get '/throw_2/:player_id/:token_id' do
+  @north_player = User.find(params[:player_id])
+  @north_token = Token.find(params[:token_id])
+  erb :throw_2
+end
+
